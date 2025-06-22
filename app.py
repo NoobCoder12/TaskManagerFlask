3dee35e08b201efc9416b8a6a0f5f49af2957e4d
NoobCoder12 <jakubszrajber12@gmail.com>
2025-06-22 19:31:16 +0200

    Update app.py
    adding toasts

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from logic.task_manager import TaskManager
+import os
+
 
 app = Flask(__file__)
 manager = TaskManager()
-
-
+app.secret_key = os.urandom(24)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -31,14 +32,19 @@ def update_task(task_name):
 
 @app.route('/save/', methods=['GET', 'POST'])
 def save_list():
-    manager.save_list()
-    return redirect('/')
+    if manager.task_list:
+        manager.save_list()
+        flash('List was saved.', 'success')
+        return redirect('/')
+    else:
+        flash('Cannot save list - add a task first.', 'error')
+        return redirect('/')
+
 
 @app.route('/load/', methods=['GET', 'POST'])
 def load_list():
     manager.load_list()
     return redirect('/')
-
 
 
 
